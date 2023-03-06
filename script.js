0e0b83b9d3cd4d227b4a0f1684bc41834a33e269
Bello Toybat <98482637+Tybello-123@users.noreply.github.com>
2023-03-06 23:41:02 +0100

    First Working commit

--- script.js
+++ script.js
@@ -1,24 +1,116 @@
-const addOneBtn = document.getElementById('one-btn'); 
-const addTwoBtn = document.getElementById('two-btn'); 
-const addThreeBtn = document.getElementById('three-btn');
+// const addOneBtn = document.getElementById('one-btn'); 
+// const addTwoBtn = document.getElementById('two-btn'); 
+// const addThreeBtn = document.getElementById('three-btn');
+
+// const addOneBtnG = document.getElementById('g-one-btn'); 
+// const addTwoBtnG = document.getElementById('g-two-btn'); 
+// const addThreeBtnG = document.getElementById('g-three-btn');
+
+
 const homeCounter = document.getElementById('h-count');
 const guestCounter = document.getElementById('g-count');
 
 
-let count = 0;
+
+const newGameBtn = document.getElementById("new-game");
+const saveGameBtn = document.getElementById("save-game");
+const endGameBtn = document.getElementById("end-game");
+
+
+
+
+let homeCount = 0;
 
 const addOne = () => {
-    count = count + 1
-homeCounter.textContent = count;
-guestCounter.textContent = count;
+   homeCount = homeCount + 1
+homeCounter.textContent = homeCount;
+  
 }
 
 const addTwo = () => {
-    count = count + 2
-homeCounter.textContent = count;
+    homeCount = homeCount + 2
+homeCounter.textContent = homeCount;
+ 
 }
 
 const addThree = () => {
-    count = count + 3
-homeCounter.textContent = count;
+homeCount = homeCount + 3
+homeCounter.textContent = homeCount;
+
 }
+
+
+
+let guestCount = 0;
+
+const addOneGuest = () => {
+    guestCount = guestCount + 1
+guestCounter.textContent = guestCount;
+
+}
+
+const addTwoGuest = () => {
+    guestCount = guestCount + 2
+guestCounter.textContent = guestCount;
+}
+
+const addThreeGuest = () => {
+    guestCount = guestCount + 3
+guestCounter.textContent = guestCount;
+}
+
+
+//Display Messages
+let displayHeading = document.getElementById('d-heading')
+
+const endGame = () => {
+    if (homeCount > guestCount) {
+        homeCounter.style.backgroundColor = 'green'; 
+        guestCounter.style.backgroundColor = 'red'; 
+        displayHeading.textContent += " : Home Wins"
+
+    } else if(homeCount < guestCount) {
+
+        homeCounter.style.backgroundColor = 'red'; 
+        guestCounter.style.backgroundColor = 'green'; 
+        displayHeading.textContent += " : Guest Wins";
+
+    }else{
+        homeCounter.style.backgroundColor = 'red'; 
+        guestCounter.style.backgroundColor = 'red'; 
+        displayHeading.textContent += " : It's A tie";
+    }
+}
+
+
+let saveHomeCount;
+let saveGuestCount;
+let getHomeCount = document.getElementById("save-home")
+let getGuestCount = document.getElementById("save-guest")
+
+const saveGame = () => {
+
+    saveHomeCount = homeCount + " -- ";
+    getHomeCount.textContent += saveHomeCount;
+
+    saveGuestCount = guestCount + " -- ";
+    getGuestCount.textContent += saveGuestCount;
+}
+
+
+const newGame = () => {
+
+   
+ 
+    homeCounter.textContent = 0;
+    guestCounter.textContent = 0;
+
+    getHomeCount.textContent = "";
+    getGuestCount.textContent = "";
+
+    displayHeading.textContent = "Basketball ScoreBoard🏀🏀";
+    homeCounter.style.backgroundColor = 'blue'; 
+    guestCounter.style.backgroundColor = 'blue'; 
+    
+}
+
